5b7b9bf2c566e91200374ba711eeef36cee3c110
Etskh <etskh@hotmail.com>
2016-08-25 01:33:54 -0700

    Changed treegen into its own repo

--- druid.js
+++ druid.js
@@ -1,40 +1,40 @@
-'use strict';
+'use strict'
 
-var express = require('express');
-var fs = require('fs');
+//var express = require('express')
+//const fs = require('fs')
 
-var treegen = require('./treegen/treegen');
+const treegen = require('druid-treegen')
 
-var config = {
+const config = {
   baseLength: 15.0,
   widthHeightRatio: 0.3,
   maxEnergy: 10.0,
   branchEnergyRatio: 0.5,
-};
+}
+
+
 treegen.generate(config, function( tree ){
-  console.log(JSON.stringify(tree));
-});
+  console.warn(JSON.stringify(tree))
+})
 
 /*var app = express();
-
-
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res){
-  res.send(fs.readFileSync('server/views/index.html', 'utf8'));
+	res.send(fs.readFileSync('server/views/index.html', 'utf8'));
 });
 
 app.get('/assets/tree/generate', function(req, res){
-  var config = {
-    baseLength: 15.0,
-    widthHeightRatio: 0.3,
-    maxEnergy: 10.0,
-    branchEnergyRatio: 0.5,
-  };
-  treegen.generate(config, function( tree ){
-    res.send( JSON.stringify(tree));
-  });
+	var config = {
+		baseLength: 15.0,
+		widthHeightRatio: 0.3,
+		maxEnergy: 10.0,
+		branchEnergyRatio: 0.5,
+	};
+	treegen.generate(config, function( tree ){
+		res.send( JSON.stringify(tree));
+	});
 });
 
 
--- treegen/treegen.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var tree = require("./build/Release/tree");
-
-module.exports.generate = function( config, cb ) {
-  cb( tree.generate(config));
-};
